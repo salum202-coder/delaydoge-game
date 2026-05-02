141a895bd5695bfe421ee5845f1893b750c72ecb
salum202-coder <salum202@gmail.com>
2026-05-02 10:33:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
+app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use(express.static(path.join(__dirname)));
 const PORT = process.env.PORT || 10000;
 const MONGO_URI = process.env.MONGO_URI;
 const BOT_TOKEN = process.env.BOT_TOKEN;
